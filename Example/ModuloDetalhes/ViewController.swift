e31929b99add53f73a9feae7673ae59208aea0e2
Jonattan <jonattan.sousa@gmail.com>
2021-04-15 17:11:21 -0300

    Criando funcionalidade de exibição a partir do módulo

--- Example/ModuloDetalhes/ViewController.swift
+++ Example/ModuloDetalhes/ViewController.swift
@@ -7,18 +7,62 @@
 //
 
 import UIKit
+import ModuloDetalhes
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackMain: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupUI() {
+        let head = CoinDetalheUIView.fromNib()
+        head.setupUI(delegate: self)
+        
+        stackMain.addArrangedSubview(head)
     }
 
 }
 
+extension ViewController: CoinDetalheDelegate {
+    func favoritar() {
+        // Ação que vc quiser
+    }
+    
+    
+}
+
+extension UIView {
+    public func loadNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nibName = type(of: self).description().components(separatedBy: ".").last!
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        guard let nibzao = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return UIView()}
+        return nibzao
+    }
+}
+
+extension UIView {
+    public static var bundleUI: Bundle {
+        var bundle: Bundle
+        if let bundeLet = Bundle(identifier: "org.cocoapods.demo.ModuloDetalhes-Example") {
+            bundle = bundeLet
+        } else {
+            bundle = Bundle(for: self)
+        }
+        return bundle
+    }
+    public class func fromNib() -> Self {
+        return fromNib(viewType: self)
+    }
+    public class func fromNib<T: UIView>(viewType: T.Type) -> T {
+        if let nib = bundleUI.loadNibNamed(String(describing: viewType), owner: nil, options: nil)?.first as? T {
+            return nib
+        }
+        return T()
+    }
+}
---
+++ Example/Pods/Target Support Files/ModuloDetalhes/ModuloDetalhes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ModuloDetalhes : NSObject
+@end
+@implementation PodsDummy_ModuloDetalhes
+@end
